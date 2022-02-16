1af7354edd096db5a70dfc0c3c61bccb8e7b2c3e
Will <Casey>
2022-02-15 20:56:05 -0700

    added jest

---
+++ server/src/tests/book.test.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import { ApolloServer, gql } from 'apollo-server';
+import schema from '@src/schemas/_schema';
+
+const testServer = new ApolloServer({ schema });
+
+describe('resolvers', () => {
+    const newBook = { title: 'The DaVinci Code', author: 'Dan Brown' };
+
+    it('createBook mutation', async () => {
+
+        const createBook = `
+            mutation CreateBook($title: String!, $author: String!) {
+                createBook(title: $title, author: $author)
+                    id
+                    title
+                    author
+            }
+        `;
+
+        const res = await testServer.executeOperation({
+            query: createBook,
+            variables: newBook
+        });
+
+        expect(res.data).toEqual({ 
+            createBook: { 
+                id: expect.stringMatching('[a-z0-9]*'),
+                title: newBook.title, 
+                author: newBook.author 
+            } 
+        });
+    });
+
+    it('getBooks query', async () => {
+
+        const getBooks = `
+            query GetBooks{
+                getBooks {
+                    id
+                    title
+                    author
+                }
+            }
+        `;
+
+        const getBooksResponse = await testServer.executeOperation({
+            query: getBooks
+        });
+
+        expect(getBooksResponse).toHaveProperty('data.getBooks');
+    });
+});
