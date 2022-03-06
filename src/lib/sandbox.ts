ed384e071f616e381cd9d1d43b4c1c7675248fb0
Will <will.casey5@gmail.com>
2022-03-06 09:55:33 -0700

    remove unneeded deps

--- src/lib/sandbox.ts
+++ src/lib/sandbox.ts
@@ -1,11 +1,8 @@
 import { Bag } from '@models/Bag'
 import { Item } from '@models/Item'
-import { Client } from '@notionhq/client'
 import schema from '@schemas/_schema'
 import { ApolloServer } from 'apollo-server-express'
 import 'dotenv/config'
-
-const notion = new Client({ auth: process.env.NOTION_API_KEY })
 
 const server = new ApolloServer({ schema })
 
